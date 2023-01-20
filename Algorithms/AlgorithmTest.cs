24f88b3e3eaeab5456822e35a991274d4a0ef9f6
vbossio <vbossio@comcast.net>
2023-01-20 11:12:53 -0500

    Initial changes to Algorithms

--- Algorithms/AlgorithmTest.cs
+++ Algorithms/AlgorithmTest.cs
@@ -1,19 +1,45 @@
+using System;
 using Xunit;
 
 namespace DeveloperSample.Algorithms
 {
     public class AlgorithmTest
     {
-        [Fact(Skip="Not implemented")]
+        [Fact]
         public void CanGetFactorial()
         {
             Assert.Equal(24, Algorithms.GetFactorial(4));
+            Assert.Equal(3628800, Algorithms.GetFactorial(10));
+            Assert.Equal(479001600, Algorithms.GetFactorial(12));
+        }
+        [Fact]
+        public void ExceptionFactorialLessThanOne()
+        {
+            var exception = Record.Exception(() => Algorithms.GetFactorial(-4));
+            Assert.NotNull(exception); 
+            Assert.IsType<InvalidOperationException>(exception);
+            Assert.Equal(Algorithms.FACTORIAL_NOT_SUPPORTED, exception.Message);
         }
 
-        [Fact(Skip="Not implemented")]
+        [Fact]
+        public void ExceptionInvalidFactorialForInt()
+        {
+
+            var exception = Record.Exception(() => Algorithms.GetFactorial(13));
+            Assert.NotNull(exception);
+            Assert.IsType<OverflowException>(exception);
+        }
+
+        [Fact]
         public void CanFormatSeparators()
         {
             Assert.Equal("a, b and c", Algorithms.FormatSeparators("a", "b", "c"));
+            Assert.Equal("a and b", Algorithms.FormatSeparators("a", "b"));
+            Assert.Equal("a", Algorithms.FormatSeparators("a"));
+            Assert.Equal("", Algorithms.FormatSeparators(""));
+            Assert.Equal("a, b, c and d", Algorithms.FormatSeparators("a", "b", "c", "d"));
+            Assert.Equal("Me,first, Me,second and Me,third/last", Algorithms.FormatSeparators("Me,first", "Me,second", "Me,third/last"));
+            Assert.Equal("a,b,c and d,e,f", Algorithms.FormatSeparators("a,b,c", "d,e,f"));
         }
     }
 }
--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -1,11 +1,61 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public const string FACTORIAL_NOT_SUPPORTED = "Factorial less than 1 is not supported";
+        public static int GetFactorial(int n)
+        {
+            if (n < 1)
+            {
+                // note: this may still interrupt the test, if so uncheck the "Break on this user-handled exception" option
+                throw new InvalidOperationException(FACTORIAL_NOT_SUPPORTED);
+            }
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+            int result = n;
+            try
+            {
+                while (n > 1)
+                {
+                    checked
+                    {
+                        result *= (n - 1);
+                    }
+                    n--;
+                }
+            } catch (OverflowException e)
+            {
+                // note: this may still interrupt the test, if so uncheck the "Break on this user-handled exception" option
+                throw e;
+            }
+            return result;
+        }
+
+        public static string FormatSeparators(params string[] items)
+        {
+            string formattedItems = "";
+            var totalCount = items.Length;
+            if (totalCount == 1)
+            {
+                return items[0];
+            }
+            for (int count = 0; count < totalCount; count++)
+            {
+                // if 2nd to last item then append with "and"
+                if (count + 2 == totalCount)
+                {
+                    formattedItems += items[count] + " and ";
+                } else if (count + 1 == totalCount)  // if last item just append value
+                {
+                    formattedItems += items[count];
+                } else
+                {
+                    formattedItems += items[count] + ", "; // otherwise append value and comma
+                }
+            }
+            return formattedItems;
+        }
     }
 }
